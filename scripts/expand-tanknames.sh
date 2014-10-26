188ae2031a3c72213ba46dc727f9e7d772d75c18
Dylan W. Schwilk <dylan@schwilk.org>
2014-10-26 17:13:39 -0500

    Modify example scripts

--- scripts/expand-tanknames.sh
+++ scripts/expand-tanknames.sh
@@ -6,10 +6,10 @@ EXPANDED_NAMES="../results/expanded-tank-tree-names.txt"
 python synonymize.py -a expand $TANKNAMES > $EXPANDED_NAMES
 
 # to merge the result, in $EXPANDED NAMES
-# python synonymize.py -a merge -c $TANKNAMES $EXPANDED_NAMES >> ../results/merged-names.txt
+# python synonymize.py -a merge -c $TANKNAMES $EXPANDED_NAMES > ../results/merged-names.txt
 
 # proof this is reversible :
-# diff -s <(sort $TANKNAMES) <(python synonymize.py -a merge -c $TANKNAMES $EXPANDED_NAMES | sort | uniq)
+diff -s <(sort $TANKNAMES) <(python synonymize.py -a merge -c $TANKNAMES $EXPANDED_NAMES | sort | uniq)
 
 ## output: Files /dev/fd/63 and /dev/fd/62 are identical
 
